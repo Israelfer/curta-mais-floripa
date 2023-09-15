72cb8570bc4dce920e2aa0b3007c910c195e52d5
Israel Batista <israel.fbb@gmail.com>
2023-09-14 23:47:28 -0300

    criando estrutura paginas: praias e sobre

---
+++ src/pages/Praias/Praias.tsx
@@ -0,0 +1,19 @@
+import { Footer } from "../../components/Footer/Footer";
+import { Header } from "../../components/Header/Header";
+import "./style.css";
+
+export function Praias() {
+  return (
+    <>
+      <Header />
+      <section>
+        <div>
+          <h1>Praias em Floripa</h1>
+          <p>Veja detalhes de cada praia e saiba como curtir ao máximo!</p>
+        </div>
+        <div></div>
+      </section>
+      <Footer />
+    </>
+  );
+}
--- src/pages/Sobre/Sobre.tsx
+++ src/pages/Sobre/Sobre.tsx
@@ -1,7 +1,24 @@
+import CidadeFloripa from "../../assets/sobre-florianopolis.jpg";
+import "./style.css";
+
 export function Sobre() {
   return (
-    <div>
-      <p>Sobre</p>
-    </div>
+    <section className="sobreSection">
+      <img src={CidadeFloripa} alt="Cidade de Florianópolis" />
+      <div>
+        <h1>Sobre</h1>
+        <h3>O que Floripa tem de bom?</h3>
+        <p>
+          Florianópolis tem encantos que atraem turistas do mundo inteiro:
+          praias para todos os gostos, culinária variada onde os frutos do mar
+          são o carro chefe e muita história pra ser vista e ouvida. Por serem
+          tantas as atrações, pode pintar a dúvida de quem vem pela primeira
+          vez: o que fazer em Floripa?
+        </p>
+        <h4>
+          Encontre aqui as melhores informações sobre as praias de Floripa
+        </h4>
+      </div>
+    </section>
   );
 }
