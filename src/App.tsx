8ce3c73e23e79df94d8682cf9b6d1d94607cb669
Israel Batista <israel.fbb@gmail.com>
2023-09-16 20:36:40 -0300

    resolvendo problema de importacao, p/ deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Inicio } from "./pages/Inicio/Inicio";
 import { Praias } from "./pages/Praias";
 import { NoPage } from "./pages/NoPage";
-// import { Footer } from "./components/Footer/Footer";
 
 export function App() {
   return (
@@ -12,8 +11,7 @@ export function App() {
           <Route path="/" element={<Inicio />} />
           <Route path="praias" element={<Praias />} />
           <Route path="*" element={<NoPage />} />
-       </Routes>
-       <Footer/>
+        </Routes>
       </BrowserRouter>
     </>
   );
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,25 +1,28 @@
-import { FaFacebook, FaInstagram, FaLinkedin, FaWhatsapp } from 'react-icons/fa';
-import './index.css';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaWhatsapp,
+} from "react-icons/fa";
+import "./index.css";
 
-function Footer() {
+export function Footer() {
   return (
-   <footer className='footer'>
-    <ul className='social_list'>
-      <li>
-        <FaFacebook />
+    <footer className="footer">
+      <ul className="social_list">
+        <li>
+          <FaFacebook />
         </li>
-      <li>
-        <FaInstagram />
-      </li>
-      <li>
-        <FaLinkedin/>
-      </li>
-      <li>
-        <FaWhatsapp/>
-      </li>
-    </ul>
-   </footer>
+        <li>
+          <FaInstagram />
+        </li>
+        <li>
+          <FaLinkedin />
+        </li>
+        <li>
+          <FaWhatsapp />
+        </li>
+      </ul>
+    </footer>
   );
 }
-
-export default Footer
