b29ba52a5aa1379a326addf1e6d0b7d9a17c33ee
Israel Batista <israel.fbb@gmail.com>
2023-09-18 21:08:48 -0300

    remove readme na pasta frontend, css noPage

--- frontend/src/pages/NoPage/index.tsx
+++ frontend/src/pages/NoPage/index.tsx
@@ -5,8 +5,10 @@ export function NoPage() {
   return (
     <>
       <Navbar />
-      <h1>Erro 404</h1>
-      <p>Página não encontrada.</p>
+      <div style={{ textAlign: "center", padding: "50px 0" }}>
+        <h1>Erro 404</h1>
+        <p>Página não encontrada.</p>
+      </div>
       <Footer />
     </>
   );
